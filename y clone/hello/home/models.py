721fff899883486d999db6748aabc752745b739b
Akshaylasure <akshaylasure318121@gmail.com>
2024-06-02 10:45:39 +0530

    Rename the image to video

--- y clone/hello/home/models.py
+++ y clone/hello/home/models.py
@@ -5,7 +5,7 @@ class Video(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     time = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='video/images/', default='video/images/default.jpg')
+    video = models.ImageField(upload_to='video/images/', default='video/images/default.jpg')
 
     def __str__(self):
         return self.name
